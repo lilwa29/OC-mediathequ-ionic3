c231e45a01ffa22a227932f17639510856666172
lilwa29 <laura.daras@gmail.com>
2019-01-22 21:08:52 +0100

    Lists et Modals pour Books et Cds

---
+++ src/modeles/Cd.model.ts
@@ -0,0 +1,6 @@
+export class CdModel {
+  isLent: boolean;
+  constructor(public title: string, public singer: string){
+    this.isLent = false;
+  }
+}
--- src/modeles/Cd.modele.ts
+++
@@ -1,6 +0,0 @@
-export class CdModele {
-  isLent: boolean;
-  constructor(public title: string, public singer: string, isLent: boolean){
-    this.isLent = false;
-  }
-}
